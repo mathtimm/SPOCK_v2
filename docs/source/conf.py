f1548c72d063dbab2c5a393e3435b07f6ac024e6
educrot22 <ducrotelsa@gmail.com>
2024-10-22 19:46:36 +0200

    conf old

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,25 @@ release = '2.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 # Sphinx extensions
-extensions = ['sphinx_rtd_theme', 'sphinx.ext.todo', 'sphinx.ext.viewcode',
-              'sphinx.ext.autodoc', 'numpydoc', 'nbsphinx', 'myst_parser',
-              'sphinx.ext.autosectionlabel', 'sphinx.ext.napoleon',]
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
+    'jupyter_sphinx',
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+    ]
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+
+autosummary_generate = False
+autodoc_default_flags = ['members']
 
 # Templates and static file directories
 html_static_path = ['_static']  # Directory for static files like CSS
@@ -31,20 +47,44 @@ html_theme = 'sphinx_rtd_theme'  # Read the Docs theme
 
 import sphinx_rtd_theme
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# Theme options
+html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
-    'style_external_links': True,
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#007541',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
 
-# Paths to custom CSS files
-html_css_files = [
-    'css/theme.css',  # Add your custom CSS file if needed
-]
+html_static_path = []
 
-html_js_files = [
-    'js/custom.js',
-]
+# Custom sidebar templates, must be a dictionary that maps document names
+# to template names.
+#
+# This is required for the alabaster theme
+# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
+html_sidebars = {
+    '**': [
+        'relations.html',  # needs 'show_related': True theme option to display
+        'searchbox.html',
+    ]
+}
+
+source_suffix = '.rst'
+# The master toctree document.
+master_doc = 'index'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
