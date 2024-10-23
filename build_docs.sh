67f4ef81aa21307a34923f56f754f4f6eea2933a
educrot22 <ducrotelsa@gmail.com>
2024-10-23 10:15:23 +0200

    new try

--- build_docs.sh
+++ build_docs.sh
@@ -4,10 +4,10 @@
 cd docs || exit
 
 # Build the documentation
-sphinx-build -b html source/ build/html
+sphinx-build -b html docs/ docs/_build/html
 
 # Copy the built HTML files to the docs root
-cp -r build/html/* ./
+cp -r docs/_build/html/* ./docs
 
 # Clean up the build directory (optional)
-rm -rf build/html
+rm -rf docs/_build/html
--- docs/source/content/api.rst
+++
@@ -1,5 +0,0 @@
-.. _api:
-
-.. autoclass:: SPOCK.long_term_scheduler
-    :members:
-    :inherited-members:
