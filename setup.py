81380eb0e7f23a9d28b96d2c03ea05740e5f4b56
educrot22 <ducrotelsa@gmail.com>
2024-11-16 23:31:27 +0100

    push

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'SPOCK',
+    version = '2.0',
+    author = 'Elsa Ducrot',
+    author_email = 'ducrotelsa@gmail.com',
+    description = ('Speculoos Observatory SChedule maKer for chilean night on SPECULOOS South Observatory'),
+    keywords = '',
+    url = 'https://github.com/educrot22/SPOCK/',
+    packages = find_packages(),
+    long_description = read('README.rst'),
+    python_requires='>=3.6',
+    install_requires =["numpy>=1.24.0"],
+)
